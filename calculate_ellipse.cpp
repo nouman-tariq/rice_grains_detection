e79094ead67a661efe997ad60a1a6f2b8c459aad
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2020-11-19 21:54:36 +0500

    adding 2 more functions

---
+++ calculate_ellipse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Ellipse
+{
+    float x = 0;
+    float y = 0;
+    float theta = 0;
+    float w = 0;
+    float l = 0;
+    float x1 = 0;
+    float y1 = 0;
+    float x2 = 0;
+    float y2 = 0;
+};
+
+int main()
+{  
+    // input M is also a structure with almost 6 parameters.
+    Ellipse E;
+    
+
+}
---
+++ process_object.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    uint8_t serialized_object_data[3080][3080], tempdata;
+    // size of serialized_object_data should be sorted
+    // serialized_object_data should be a pointer as well!
+    tempdata = serialized_object_data[0][0] + 1;
+}
