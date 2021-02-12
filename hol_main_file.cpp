03c349e7c0e48611c393cbb73b4a1795c6a7d1d5
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2021-02-12 21:16:23 +0500

    working on array2uint

--- hol_main_file.cpp
+++ hol_main_file.cpp
@@ -51,7 +51,7 @@ void process_object(uint8_t serialized_object_data[][4000]);
 moments label_window(int burstpos, int wdwidx, int east, Mat img);
 void RGB2HSV(double R, double G, double B, double *HSV);
 uint8_t * serialize_object(int &out_size);
-// void deserialize_object(uint8_t *indata);
+void deserialize_object(uint8_t *indata, int &out_size);
 void get_scan(Mat img, int wdw_row);
 void image_read();
 void uint2array(uint32_t ID, int numbytes, uint8_t *hdr_id);
@@ -129,8 +129,7 @@ int main(int argc, char **argv)
 				ellipse[label_index] = calculate_ellipse(mm);
 				ellipse[label_index].y = (ellipse[label_index].y + rowcount) - 1;
 				label_index += 1;
-
-				// 			// deserialize_object();
+				deserialize_object(outobject, out_size);
 				// 			// check_connected;
 				// 			// separate_connected_grains();
 			}
@@ -686,6 +685,77 @@ uint8_t * serialize_object(int &out_size)
 	}
 	return outdata;
 }
+void deserialize_object(uint8_t *indata, int &out_size)
+{
+	
+/*	
+	int label, num_rows, object_width, data_index, num_sections, col_start, section_size;
+    uint8_t out_image[num_rows][object_width][3] = {}; // verify initialized to 0 or not!
+    uint8_t label_arr[4], num_rows_arr[2], object_width_arr[2],num_sections_arr[2],col_start_arr[2], section_size_arr[2] ,indata[3080][3080];
+
+    for (size_t i = 0; i < 4; i++)
+    {
+        label_arr[i] = indata[i][0];
+    }
+    label = array2uint(label_arr);
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        num_rows_arr[i] = indata[i+4][0];
+    }
+    num_rows = array2uint(num_rows_arr);
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        object_width_arr[i] = indata[i+6][0];
+    }
+    object_width = array2uint(object_width_arr);
+
+    data_index = OBJHDRSIZE + 1;
+
+    // out_image initialization to zero.
+
+    for (size_t row = 0; row < num_rows; row++)
+    {
+    
+        for (size_t i = 0; i < 2; i++)
+        {
+            num_sections_arr[i] = indata[data_index+i][0];
+        }
+        num_sections = array2uint(num_sections_arr);
+
+        data_index = ROWHDRSIZE + data_index;
+        for (size_t i = 0; i < num_sections; i++)
+        {
+            
+            for (size_t i = 0; i < 2; i++)
+            {
+                col_start_arr[i] = indata[data_index+i][0];
+            }
+            col_start = array2uint(col_start_arr);
+
+            
+            for (size_t i = 0; i < 2; i++)
+            {
+                section_size_arr[i] = indata[data_index+2+i][0];
+            }
+            section_size = array2uint(section_size_arr);
+
+            data_index = SECTIONHDRSIZE + data_index;
+            for (size_t i = 0; i < section_size-1; i++)
+            {
+                out_image[row][col_start+i][0] = indata[data_index][0];
+                out_image[row][col_start+i][1] = indata[data_index+1][0];
+                out_image[row][col_start+i][2] = indata[data_index+2][0];
+                data_index = data_index + 3;
+            }
+            
+        }
+        
+
+    }
+*/   
+}
 void get_scan(Mat img, int wdw_row)
 {
 
@@ -803,7 +873,36 @@ void uint2array(uint32_t num, int numbytes, uint8_t *hdr_id)
 	}
 
 }
+void array2uint(uint8_t *inarray, int array_size)
+{
+	switch (array_size)
+	{
+	case 1:
+		uint8_t outcast_8;
+		outcast_8 = uint8_t (num);
+		hdr_id[0] = (uint8_t) outcast_8;
+		break;
+	case 2:
+		uint16_t outcast_16;
+		outcast_16 = uint16_t (num);
+		hdr_id[0] = (uint8_t) outcast_16;
+		break;
+	case 4:
+		uint32_t outcast_32;
+		outcast_32 = uint32_t (num);
+		hdr_id[0] = (uint8_t) outcast_32;
+		break;
+	case 8:
+		uint64_t outcast_64;
+		outcast_64 = uint64_t (num);
+		hdr_id[0] = (uint8_t) outcast_64;
+		break;
+	default:
+		cout<<"Invalid data type entered- "<<endl;
+		break;
+	}
 
+}
 
 // g++ -g hol_main_file.cpp -o hol_main_file -std=c++11 `pkg-config --cflags --libs opencv`
 // ./testoutput
